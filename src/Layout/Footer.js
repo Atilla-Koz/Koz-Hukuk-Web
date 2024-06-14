1f4cc9506ccc7865ca82fad179126815ef4ec3c3
Atilla-Koz <atillakoz@hotmail.com>
2024-06-14 14:40:50 +0300

    chore: Refactor Footer and Header components

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,5 +1,7 @@
 export default function Footer() {
-  <section>
-    <h1>Footer</h1>
-  </section>;
+  return (
+    <section>
+      <h1>Footer</h1>
+    </section>
+  );
 }
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,7 +1,35 @@
+import Logo from '../assets/Header/KHLogo.svg';
+import search from '../assets/Header/search.svg';
+import bars from '../assets/Header/bars.svg';
+import norway from '../assets/Header/norway.mp4';
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="">
+      <section className="flex flex-col justify-between p-8">
+        <div className="absolute inset-0 z-0">
+          <video
+            className="w-full h-[1000px] object-cover"
+            src={norway}
+            autoPlay
+            loop
+            muted
+          />
+        </div>
+        <div className="flex justify-between items-center z-10 relative w-full">
+          <img className="w-[80px]" src={Logo} alt="Logo" />
+          <div className="flex flex-row gap-8">
+            <img className="w-[30px] text-white" src={search} alt="Search" />
+            <img className="w-[30px] text-white" src={bars} alt="Menu" />
+          </div>
+        </div>
+      </section>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-start">
+        <h2 className="">Bilgi</h2>
+        <h2>Deneyim</h2>
+        <h2>Takım Çalışması</h2>
+        <button className="pt-8">Keşfet</button>
+      </div>
     </header>
   );
 }
