6cd62f4e22b026c51479ea12f6c49e56202e7ad5
Atilla-Koz <atillakoz@hotmail.com>
2024-07-11 19:49:34 +0300

    Refactor ClientsSay component and update homepage layout

--- src/Layout/Components/HomePage/ClientsSay.js
+++ src/Layout/Components/HomePage/ClientsSay.js
@@ -2,32 +2,38 @@ export default function ClientsSay() {
   return (
     <section className="bg-[#252128] bg-cover bg-center min-h-screen flex flex-col gap-4 justify-center items-center">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h3 className="text-white">What Our Clients Say About Us</h3>
-        <h4 className="text-[#ff6e0d]">READ OUR 5-STAR REVİEWS</h4>
+        <h3 className="text-white lg:text-[60px]">
+          What Our Clients Say About Us
+        </h3>
+        <h4 className="text-[#ff6e0d] lg:text-[40px]">
+          READ OUR 5-STAR REVİEWS
+        </h4>
       </div>
-      <div className="flex flex-col items-start gap-6 pt-16 pl-16 pr-16">
-        <h4 className="text-[#ff6e0d] font-bold">- Chi Banh</h4>
-        <p className="text-white font-bold">
-          Boost your product and service's credibility by adding testimonials
-          from your clients. People love recommendations so feedback from others
-          who've tried it is invaluable.
-        </p>
-      </div>
-      <div className="flex flex-col items-start gap-6 pt-16 pl-16 pr-16">
-        <h4 className="text-[#ff6e0d] font-bold">- Chi Banh</h4>
-        <p className="text-white font-bold">
-          Boost your product and service's credibility by adding testimonials
-          from your clients. People love recommendations so feedback from others
-          who've tried it is invaluable.
-        </p>
-      </div>
-      <div className="flex flex-col items-start gap-6 pt-16 pl-16 pr-16">
-        <h4 className="text-[#ff6e0d] font-bold">- Chi Banh</h4>
-        <p className="text-white font-bold">
-          Boost your product and service's credibility by adding testimonials
-          from your clients. People love recommendations so feedback from others
-          who've tried it is invaluable.
-        </p>
+      <div className="lg:flex lg:flex-row ">
+        <div className="flex flex-col items-start gap-6 pt-16 pl-16 pr-16 lg:flex-col-reverse">
+          <h4 className="text-[#ff6e0d] font-bold">- Chi Banh</h4>
+          <p className="text-white font-bold lg:text-[20px]">
+            Boost your product and service's credibility by adding testimonials
+            from your clients. People love recommendations so feedback from
+            others who've tried it is invaluable.
+          </p>
+        </div>
+        <div className="flex flex-col items-start gap-6 pt-16 pl-16 pr-16 lg:flex-col-reverse">
+          <h4 className="text-[#ff6e0d] font-bold">- Chi Banh</h4>
+          <p className="text-white font-bold lg:text-[20px]">
+            Boost your product and service's credibility by adding testimonials
+            from your clients. People love recommendations so feedback from
+            others who've tried it is invaluable.
+          </p>
+        </div>
+        <div className="flex flex-col items-start gap-6 pt-16 pl-16 pr-16 lg:flex-col-reverse">
+          <h4 className="text-[#ff6e0d] font-bold">- Chi Banh</h4>
+          <p className="text-white font-bold lg:text-[20px]">
+            Boost your product and service's credibility by adding testimonials
+            from your clients. People love recommendations so feedback from
+            others who've tried it is invaluable.
+          </p>
+        </div>
       </div>
     </section>
   );
