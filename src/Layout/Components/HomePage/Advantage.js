2337cb749c53a71f9dbd3b900f41341fd2939d29
Atilla-Koz <atillakoz@hotmail.com>
2024-07-11 19:34:33 +0300

    chore: Update Advantage component layout and styles

--- src/Layout/Components/HomePage/Advantage.js
+++ src/Layout/Components/HomePage/Advantage.js
@@ -2,44 +2,51 @@ import balance from '../../../assets/Advantege/balance.svg';
 
 export default function Advantage() {
   return (
-    <section className="bg-white bg-cover bg-center min-h-screen flex flex-col gap-8 justify-center items-center">
-      <div>
-        <h3>The Arivaci & Co. advantage</h3>
-        <hr className="w-[20rem] border-[#ff6e0d] border-2" />
+    <section className="bg-[#FDFAF5] bg-cover bg-center min-h-screen flex flex-col gap-8 justify-center items-center lg:flex-row lg:gap-24 ">
+      <div className="lg:ml-8 lg:pr-[20%]">
+        <h3 className="lg:text-[50px] text-wrap">The Arivaci &</h3>
+        <h3 className="lg:text-[50px] text-wrap">Co. advantage</h3>
+        <hr className=" border-[rgb(255,110,13)] border-2 lg:w-[21rem]" />
       </div>
-      <div className="flex flex-col items-center gap-4">
-        <img className="w-[5rem] h-[5rem]" src={balance} alt="balance" />
-        <div className="flex flex-col items-start gap-4 pr-8 pl-8">
-          <h4 className="text-[#ff6e0d] font-bold">YEARS OF EXPERİENCE</h4>
-          <p className="font-bold">
-            What's special about your services or offerings? Give your audience
-            a reason to choose you over your competitors. Cite it above, then
-            flesh it out here.
-          </p>
+      <div className="lg:flex lg:flex-col lg:gap-16">
+        <div className="flex flex-col items-center gap-4 lg:flex-row">
+          <img className="w-[5rem] h-[5rem]" src={balance} alt="balance" />
+          <div className="flex flex-col items-start gap-4 pr-8 pl-8">
+            <h4 className="text-[#ff6e0d] font-bold lg:text-[40px]">
+              YEARS OF EXPERİENCE
+            </h4>
+            <p className="font-bold lg:text-[20px]">
+              What's special about your services or offerings? Give your
+              audience a reason to choose you over your competitors. Cite it
+              above, then flesh it out here.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <img className="w-[5rem] h-[5rem]" src={balance} alt="balance" />
-        <div className="flex flex-col items-start gap-4 pr-8 pl-8">
-          <h4 className="text-[#ff6e0d] font-bold">
-            RECOGNİZED FOR OUR EXPERTİSE
-          </h4>
-          <p className="font-bold">
-            What's special about your services or offerings? Give your audience
-            a reason to choose you over your competitors. Cite it above, then
-            flesh it out here.
-          </p>
+        <div className="flex flex-col items-center gap-4 lg:flex-row">
+          <img className="w-[5rem] h-[5rem]" src={balance} alt="balance" />
+          <div className="flex flex-col items-start gap-4 pr-8 pl-8">
+            <h4 className="text-[#ff6e0d] font-bold lg:text-[40px]">
+              RECOGNİZED FOR OUR EXPERTİSE
+            </h4>
+            <p className="font-bold lg:text-[20px]">
+              What's special about your services or offerings? Give your
+              audience a reason to choose you over your competitors. Cite it
+              above, then flesh it out here.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <img className="w-[5rem] h-[5rem]" src={balance} alt="balance" />
-        <div className="flex flex-col items-start gap-4 pr-8 pl-8">
-          <h4 className="text-[#ff6e0d]  font-bold">FROM START TO FİNİSH</h4>
-          <p className="font-bold">
-            What's special about your services or offerings? Give your audience
-            a reason to choose you over your competitors. Cite it above, then
-            flesh it out here.
-          </p>
+        <div className="flex flex-col items-center gap-4 lg:flex-row">
+          <img className="w-[5rem] h-[5rem]" src={balance} alt="balance" />
+          <div className="flex flex-col items-start gap-4 pr-8 pl-8">
+            <h4 className="text-[#ff6e0d]  lg:text-[40px] font-bold">
+              FROM START TO FİNİSH
+            </h4>
+            <p className="font-bold lg:text-[20px]">
+              What's special about your services or offerings? Give your
+              audience a reason to choose you over your competitors. Cite it
+              above, then flesh it out here.
+            </p>
+          </div>
         </div>
       </div>
     </section>
