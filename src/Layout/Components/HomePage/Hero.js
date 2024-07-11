cd20f1d203bf5a23a9ce28baebcd4e6269e51e96
Atilla-Koz <atillakoz@hotmail.com>
2024-07-11 18:40:24 +0300

    chore: Update homepage Hero section layout and styles

--- src/Layout/Components/HomePage/Hero.js
+++ src/Layout/Components/HomePage/Hero.js
@@ -2,8 +2,8 @@ import logo from '../../../assets/Hero/K.svg';
 
 export default function Hero() {
   return (
-    <section className="bg-hero-img bg-cover bg-center min-h-screen flex justify-center items-center ">
-      <div className="flex flex-col items-start gap-16 bg-white bg-opacity-50 p-8 rounded-lg text-center w-[90%] h-[50rem]">
+    <section className="bg-hero-img bg-cover bg-center min-h-screen flex justify-center items-center lg:justify-start lg:pl-16 ">
+      <div className="flex flex-col items-start gap-16 bg-white bg-opacity-50 p-8 rounded-lg text-center w-[90%] lg:w-[25%]">
         <img src={logo} alt="Logo" className="w-48 h-48 mb-1" />
         <h2 className="text-start ">Real estate law for the modern world</h2>
         <p className="text-black font-bold text-start text-[1rem]">
