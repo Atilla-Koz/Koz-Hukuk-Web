b5fcb3cbe6e6a0c1e9c4f7ba657bb565652dd4fe
Atilla-Koz <atillakoz@hotmail.com>
2024-07-11 15:05:16 +0300

    feat: Add OurAttorneys component to the homepage layout

--- src/Layout/Components/HomePage.js
+++ src/Layout/Components/HomePage.js
@@ -1,9 +1,11 @@
 import Hero from './HomePage/Hero';
+import OurAttorneys from './HomePage/OurAttorneys';
 
 export default function HomePage() {
   return (
     <section>
       <Hero />
+      <OurAttorneys />
     </section>
   );
 }
---
+++ src/Layout/Components/HomePage/OurAttorneys.js
@@ -0,0 +1,44 @@
+export default function OurAttorneys() {
+  return (
+    <section className="bg-[#252128] bg-cover bg-center min-h-screen flex flex-col gap-4 justify-center items-center pt-[1rem] pb-[1rem] ">
+      <h2 className="text-white">Our Attorneys</h2>
+
+      <div className="flex flex-col gap-4 items-start justify-center pt-4">
+        <img
+          className="w-[20rem] h-[20rem] rounded-[1rem] border-4 border-[#ff6e0d] shadow-lg"
+          src="https://media.licdn.com/dms/image/D4D03AQGoh_YamNut-A/profile-displayphoto-shrink_800_800/0/1709456652189?e=1726099200&v=beta&t=mpZNmP77-J6jw_GSigRGb2A3_yyKqMGTtKbaj3i8-lQ"
+          alt="name=title"
+        />
+        <div>
+          <h3 className="text-white">Chun Hei Kim</h3>
+          <h4 className="text-[#ff6e0d]">Managing Partner</h4>
+        </div>
+        <hr className="w-[20rem] border-[#ff6e0d] border-2" />
+      </div>
+      <div className="flex flex-col gap-8 items-start justify-center">
+        <img
+          className="w-[20rem] h-[20rem] rounded-[1rem] border-4 border-[#ff6e0d] shadow-lg"
+          src="https://media.licdn.com/dms/image/D4D03AQGoh_YamNut-A/profile-displayphoto-shrink_800_800/0/1709456652189?e=1726099200&v=beta&t=mpZNmP77-J6jw_GSigRGb2A3_yyKqMGTtKbaj3i8-lQ"
+          alt="name=title"
+        />
+        <div>
+          <h3 className="text-white">Chun Hei Kim</h3>
+          <h4 className="text-[#ff6e0d]">Managing Partner</h4>
+        </div>
+        <hr className="w-[20rem] border-[#ff6e0d] border-2" />
+      </div>
+      <div className="flex flex-col gap-4 items-start justify-center">
+        <img
+          className="w-[20rem] h-[20rem] rounded-[1rem] border-4 border-[#ff6e0d] shadow-lg"
+          src="https://media.licdn.com/dms/image/D4D03AQGoh_YamNut-A/profile-displayphoto-shrink_800_800/0/1709456652189?e=1726099200&v=beta&t=mpZNmP77-J6jw_GSigRGb2A3_yyKqMGTtKbaj3i8-lQ"
+          alt="name=title"
+        />
+        <div>
+          <h3 className="text-white">Chun Hei Kim</h3>
+          <h4 className="text-[#ff6e0d]">Managing Partner</h4>
+        </div>
+        <hr className="w-[20rem] border-[#ff6e0d] border-2" />
+      </div>
+    </section>
+  );
+}
